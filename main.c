c2af656e29a47e710b3243f3710cbc3f745d3021
Tal Derei <Tad222@lehigh.edu>
2020-09-17 13:37:24 -0700

    General file structure and control flow for phase_1

---
+++ main.c
@@ -0,0 +1,14 @@
+/* ROOT MAIN.c */
+
+/* CONTROL FLOW: (1) main will call function (e.g. read_input_file() inside inputFile.c to read-in 
+		     input file (plaintext) from user, and sort the file's contents with an algorithm 
+		     with efficient time complexity
+		 (2) main will then call function (e.g. build_tree() inside of merkleTree.c) to build
+		     an empty merkle tree.
+		 (3) main will then call a function (e.g. populate_tree() inside of merkleTree.c) in order
+		     to start populating the tree with the set of string inputted by the user.
+		 (4) From merkleTree.c, we'll have to call the hash function (e.g. hash() inside of sha_256.c)
+		     in order to hash the input, and then placing the hash inside the specified node
+		 (5) main will call a function (printTree() inside of merkleTree.c) in order to print out the tree
+		 (6) main will call a function (freeTree() inside of merkleTree.c) in order to free memory
+		     allocated to the merkleTree and its nodes.  
---
+++ node.h
@@ -0,0 +1,22 @@
+#ifndef NODE_DEF
+#define NODE_DEF
+
+#include <stdint.h>    /* for uint*_t types */
+
+struct leaf_node {
+  uint64_t hash;
+  int value;
+};
+typedef struct leaf_node LeafNode;
+
+struct internal_node {
+  uint64_t hash;
+  string leftEdge;
+  string rightEdge;
+  struct node *leftChild;
+  struct node *rightChild;
+};
+typedef struct internal_node InternalNode;
+
+#endif
+
