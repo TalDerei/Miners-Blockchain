ac1907f0a161117b41f5517e94609ff4144d893d
Yifan She <yis223@lehigh.edu>
2020-09-18 16:27:49 +0000

    sha_256.h uploaded

--- src/sha_256.h
+++ src/sha_256.h
@@ -1,1 +1,36 @@
 /* HEADER FILE FOR SHA-256 ALGORITHM FOR ENCRYPTION */
+
+/*********************************************************************
+* Filename:   sha_256.h
+* Author:     Brad Conte (brad AT bradconte.com)
+* Copyright:
+* Disclaimer: This code is presented "as is" without any guarantees.
+* Details:    Defines the API for the corresponding SHA1 implementation.
+*********************************************************************/
+
+#ifndef sha_256_H
+#define sha_256_H
+
+/*************************** HEADER FILES ***************************/
+#include <stddef.h>
+
+/****************************** MACROS ******************************/
+#define sha_256_BLOCK_SIZE 32            // sha_256 outputs a 32 byte digest
+
+/**************************** DATA TYPES ****************************/
+typedef unsigned char BYTE;             // 8-bit byte
+typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+
+typedef struct {
+	BYTE data[64];
+	WORD datalen;
+	unsigned long long bitlen;
+	WORD state[8];
+} sha_256_CTX;
+
+/*********************** FUNCTION DECLARATIONS **********************/
+void sha_256_init(sha_256_CTX *ctx);
+void sha_256_update(sha_256_CTX *ctx, const BYTE data[], size_t len);
+void sha_256_final(sha_256_CTX *ctx, BYTE hash[]);
+
+#endif   // sha_256_H
