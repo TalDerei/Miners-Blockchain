a5902321d4b77564fbc3412575e76954d76b1443
Yifan She <yis223@lehigh.edu>
2020-10-14 09:36:34 +0000

    adjusted

--- src/printblock.c
+++ src/printblock.c
@@ -5,12 +5,12 @@
 #include <string.h>
 #include "sha256.h"
 
-void printblock(block* B_ptr){
- printf("header: \n");
- printf("PreviousHash: %s, RootHash: %s", B_ptr->header->previousHash, B_ptr->header->rootHash);
- printf("");
- printf("TimeStamp: %d, Target: %s, nonce: %d", B_ptr->header->timestamp, B_ptr->header->target, B_ptr->header->nonce);
- printf("");
- prinff("content: \n");
- print_merkle_tree(B_ptr->rootHash);
+void printblock(block* B_ptr, int ID_K, FILE* out){
+ fprintf(out, "header: \n");
+ fprintf(out, "PreviousHash: %s, RootHash: %s", B_ptr->header->previousHash, B_ptr->header->rootHash);
+ fprintf(out, "");
+ fprintf(out, "TimeStamp: %d, Target: %s, nonce: %d", B_ptr->header->timestamp, B_ptr->header->target, B_ptr->header->nonce);
+ fprintf(out, "");
+ fprinff(out, "content: \n");
+ print_merkle_tree(B_ptr->rootHash, ID_K, out);
 }
