57ee23dda05d85caaf9e4eef74d2bdfeeee825a6
Tal Derei <Tad222@lehigh.edu>
2020-11-02 16:48:55 -0800

    comparison in progress

--- src/validation.c
+++ src/validation.c
@@ -28,8 +28,10 @@ int validate_header(Block2 *block2, char actualFileNameBlock[], int counter) {
     unsigned char buffer_first_previousHash[1];    
     unsigned char buffer_not_first_previousHash[32];
     unsigned char buffer_rootHash[64];
-    unsigned char temp1[100];
-    unsigned char temp2[100];
+    unsigned char temp1_rootHash[100];
+    unsigned char temp2_rootHash[100];
+    unsigned char temp1_timestamp[100];
+    unsigned char temp2_timestamp[100];
     int buffer_timestamp[1];
     double buffer_target[1];
     unsigned int buffer_nonce[1];
@@ -59,50 +61,9 @@ int validate_header(Block2 *block2, char actualFileNameBlock[], int counter) {
         printf("\n");
     }
 
-//    exit(0);
-//     fread(buffer_rootHash, sizeof(unsigned char), sizeof(buffer_rootHash), outputBlock);
-//     printf("buffer_rootHash is: \n");
-    // for (int n = 0; n < sizeof(buffer_rootHash); n++) {
-    //     if ((unsigned char)buffer_rootHash[n] <= 0x0f) {
-    //             printf("0%x", (unsigned char)buffer_rootHash[n]);
-    //     } else{
-    //             printf("%x", (unsigned char)buffer_rootHash[n]);
-    //     }
-    // }
-    // printf("\n");
-
-    // printf("block2->rootHashHeader is: \n");
-    // for (int n = 0; n < SHA256_BLOCK_SIZE; n++) {
-    //     if ((unsigned char)block2->rootHashHeader[n] <= 0x0f) {
-    //             printf("0%x", (unsigned char)block2->rootHashHeader[n]);
-    //     } else{
-    //             printf("%x", (unsigned char)block2->rootHashHeader[n]);
-    //     }
-    // }
-    // printf("\n");
-    
-    // printf("size of block2->rootHashHeader is: %d\n", sizeof(block2->rootHashHeader));
-    // printf("size of buffer_rootHash is: %d\n", sizeof(buffer_rootHash));
-
-    // int returnValue = memcpy(block2->rootHashHeader, buffer_rootHash, sizeof(buffer_rootHash));
-    // printf("returnValue is: %d\n", returnValue);
-    // if (returnValue != 0) {
-    //     printf("*******************error with root hash\n");
-    //     // return 0;
-    // }
-    // printf("\n");
-
-
-    // exit(0);
-
-    int lengths;
+    //roothash
     fseek(outputBlock, 15, SEEK_CUR);
     fgets(buffer_rootHash, BUFFER, outputBlock);
-    lengths = strlen(buffer_rootHash);
-    if( buffer_rootHash[lengths-1] == '\n') {
-        buffer_rootHash[lengths-1] = '\0';
-    }
-    // fread(buffer_rootHash, sizeof(unsigned char), sizeof(buffer_rootHash), outputBlock);
     FILE *tempOutput; 
     tempOutput = fopen("tempOutput.txt","w+b");
     for (int n = 0; n < sizeof(buffer_rootHash); n++) {
@@ -132,40 +93,87 @@ int validate_header(Block2 *block2, char actualFileNameBlock[], int counter) {
     int i=0;
     char l;
     while((l=fgetc(tempOutput))!=EOF) {
-      temp1[i] = l;
-      printf("%c", temp1[i]);
-      i++;
-   }
-   printf("\n i is: %d\n", i);
-   printf("\n something random at line 139: %c\n", temp1[1]);
-
-    for(int jjj = 0; jjj < 32; jjj++){
-    //printf("%c\n", temp1[jjj]);
-        printf("%c", temp1[jjj]);
-
+        temp1_rootHash[i] = l;
+        printf("%c", temp1_rootHash[i]);
+        i++;
     }
-    printf("\n");
+
     printf("temp2 is: \n");
     int j =0;
-   char t;
-   while((t=fgetc(bufferOutput))!=EOF) {
+    char t;
+    while((t=fgetc(bufferOutput))!=EOF) {
+        temp2_rootHash[j] = t;
+        printf("%c", temp2_rootHash[j]);
+        j++;
+    }
+    printf("\n");
+    printf("temp1 is:\n");
+    for (int i = 0; i < sizeof(temp1_rootHash); i++) {
+        printf("%c", (unsigned char)temp1_rootHash[i]);
+    }
+    printf("\ntemp2 is:\n");
+    for (int i = 0; i < sizeof(temp2_rootHash); i++) {
+        printf("%c", (unsigned char)temp2_rootHash[i]);
+    }
+    printf("\n");
+
+    printf("size of temp1 is: %d\n", sizeof(temp1_rootHash));
+    printf("size of temp2 is: %d\n", sizeof(temp2_rootHash));
+
+    int ret = memcmp(temp1_rootHash, temp2_rootHash, 32);
+    printf("value of ret is: %d\n", ret);
+
+    if(ret != 0) {
+    printf("*******************error with roothash\n");
+    return 0;
+    }
+
+    //timestamp
+    fseek(outputBlock, 15, SEEK_CUR);
+    fgets(buffer_rootHash, BUFFER, outputBlock);
+    FILE *tempOutput; 
+    tempOutput = fopen("tempOutput.txt","w+b");
+    for (int n = 0; n < sizeof(buffer_rootHash); n++) {
+        if ((unsigned char)buffer_rootHash[n] <= 0x0f) {
+                fprintf(tempOutput, "0%c", (unsigned char)buffer_rootHash[n]);
+        } else{
+                fprintf(tempOutput, "%c", (unsigned char)buffer_rootHash[n]);
+        }
+    }
+    printf("\n");
+    FILE *bufferOutput; 
+    bufferOutput = fopen("bufferOutput.txt","w+b");
+    printf("block2->rootHash is: ****************************\n");
+    for (int i = 0; i < SHA256_BLOCK_SIZE; i++) {
+        if ((unsigned char)block2->rootHash[i] <= 0x0f) {
+                fprintf(bufferOutput, "0%x", (unsigned char)block2->rootHash[i]);
+        } else{
+                fprintf(bufferOutput, "%x", (unsigned char)block2->rootHash[i]);
+        }
+    }
+    printf("\n");
+
+    rewind(tempOutput);
+    rewind(bufferOutput);
+    printf("printing out values!!!!!!!!!!!!!!\n");
+    printf("temp1 is: \n");
+    int i=0;
+    char l;
+    while((l=fgetc(tempOutput))!=EOF) {
+        temp1[i] = l;
+        printf("%c", temp1[i]);
+        i++;
+    }
+
+    printf("temp2 is: \n");
+    int j =0;
+    char t;
+    while((t=fgetc(bufferOutput))!=EOF) {
         temp2[j] = t;
         printf("%c", temp2[j]);
         j++;
-   }
-    printf("\n");
-
-    int len1 = strlen(temp1);
-    if( temp1[31] == '\n') {
-        temp1[31] = '\0';
-    }
-
-     int len2 = strlen(temp2);
-    if( temp2[31] == '\n') {
-        temp2[31] = '\0';
     }
     printf("\n");
-
     printf("temp1 is:\n");
     for (int i = 0; i < sizeof(temp1); i++) {
         printf("%c", (unsigned char)temp1[i]);
@@ -179,64 +187,17 @@ int validate_header(Block2 *block2, char actualFileNameBlock[], int counter) {
     printf("size of temp1 is: %d\n", sizeof(temp1));
     printf("size of temp2 is: %d\n", sizeof(temp2));
 
-    int ret = memcmp(temp1, temp2, 32);
+    int ret2 = memcmp(temp1, temp2, 32);
     printf("value of ret is: %d\n", ret);
 
-    if(ret > 0) {
-      printf("str2 is less than str1");
-   } else if(ret < 0) {
-      printf("str1 is less than str2");
-   } else {
-      printf("str1 is equal to str2");
-   }
+    if(ret2 != 0) {
+    printf("*******************error with roothash\n");
+    return 0;
+    }
+    exit(0);
 
-   exit(0);
-
-
-    // printf("buffer_timestamp is: %f", buffer_timestamp);
-    // int length = strlen(buffer_timestamp);
-    // if( buffer_timestamp[length] == '\n') {
-    //     buffer_timestamp[length] = '\0';
-    // }
-    // int buffer_timestamp_converted = atoi(buffer_timestamp);
-    // printf("buffer_timestamp_converted is: %d\n", buffer_timestamp_converted); 
-    //printf("block2->timestamp is: %d\n", block2->timestamp);
-
-//     int ret2 = memcmp(buffer_timestamp, block2->timestamp, sizeof(block2->timestamp));
-//     if(ret2 > 0) {
-//       printf("str2 is less than str1");
-//    } else if(ret2 < 0) {
-//       printf("str1 is less than str2");
-//    } else {
-//       printf("str1 is equal to str2");
-//    }
-//    exit(0);
-
-    // fseek(outputBlock, 12, SEEK_CUR);
-    // char s;
-    // for (int i = 0; i < sizeof(temp); i++) {
-    //     temp[i] = fgetc(outputBlock);
-    // }
-    // printf("temp is: \n");
-    // for (int j = 0; j < sizeof(temp); j++) {
-    //     printf("%c", temp[j]);
-    // }
-    // printf("\n");
-
-    // fread(buffer_target, sizeof(double), sizeof(buffer_target), outputBlock);
 
     
-    // fread(temp1, sizeof(double), sizeof(temp1), tempOutput);
-    // fread(temp2, sizeof(double), sizeof(temp2), bufferOutput);
-
-    // printf("%f", temp1);
-    // printf("\n");
-    // printf("%f", temp2);
-    
-    // int len = strlen(temp);
-    // if( temp[len] == '\n') {
-    //     temp[len] = '\0';
-    // }
     // printf("size of block2->target is: %d\n", sizeof(block2->target));
     
     // printf("size of temp is: %d\n", sizeof(temp));
@@ -247,13 +208,13 @@ int validate_header(Block2 *block2, char actualFileNameBlock[], int counter) {
     // exit(0);
 
 
-    fread(buffer_nonce, sizeof(unsigned int), 1, outputBlock);
-    if(memcmp(block2->nonce, buffer_nonce, sizeof(unsigned int))) {
-        printf("*******************error with timestamp\n");
-        // return 0;
-    }
-    exit(0);
-    return 1;
+    // fread(buffer_nonce, sizeof(unsigned int), 1, outputBlock);
+    // if(memcmp(block2->nonce, buffer_nonce, sizeof(unsigned int))) {
+    //     printf("*******************error with timestamp\n");
+    //     // return 0;
+    // }
+    // exit(0);
+    // return 1;
 }
 
 //validate merkle tree of block
